117706f3b52bd0fdd080db0a3dccd002874f90e3
asadbek <asadbeksr@gmail.com>
2022-11-05 21:41:37 +0500

    update seo

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -9,37 +9,37 @@ export default function SEO({ title }) {
         content='width=device-width, initial-scale=1  maximum-scale=1, user-scalable=0'
       />
       <meta charSet='utf-8' />
-      <title>{title || 'Next js template'}</title>
-      <meta name='description' content='Next js' />
+      <title>{title || "Asadbek's Portfolio "}</title>
+      <meta name='description' content='Creative Portfolio' />
       <meta
         name='keywords'
-        content='Cspace, coworking, co-working, center, tashkent'
+        content='Portfolio, creative, react, next, javascript, developer, frontend, asadbek, asadbekov, asadbekovasadbek, udevs'
       />
       <meta property='og:type' content='website' />
       <meta
         property='og:title'
-        content={title || 'Next js template'}
+        content={title || "Asadbek's Portfolio"}
         key='ogtitle'
       />
-      <meta property='og:description' content='Next js' key='ogdesc' />
+      <meta property='og:description' content='Creative Portfolio' key='ogdesc' />
       <meta
         property='og:site_name'
-        content={title || 'Next js template'}
+        content={title || "Asadbek's Portfolio"}
         key='ogsitename'
       />
       <meta
         property='og:image'
-        content='https://cdn.rasta.app/rasta/aef8208b-c6fc-4db9-878b-51544ab711be'
+        // content='https://cdn.rasta.app/rasta/aef8208b-c6fc-4db9-878b-51544ab711be'
         key='ogimage'
       />
       <meta name='twitter:card' content='summary' />
-      <meta name='twitter:title' content={title || 'Next js template'} />
+      <meta name='twitter:title' content={title || "Asadbek's Portfolio"} />
       <meta name='twitter:description' content='Next js' />
-      <meta name='twitter:site' content={title || 'Next js template'} />
-      <meta name='twitter:creator' content='Udevs' />
+      <meta name='twitter:site' content={title || "Asadbek's Portfolio"} />
+      <meta name='twitter:creator' content='Asadbek Savronov' />
       <meta
         name='twitter:image'
-        content='https://cdn.rasta.app/rasta/aef8208b-c6fc-4db9-878b-51544ab711be'
+        // content='https://cdn.rasta.app/rasta/aef8208b-c6fc-4db9-878b-51544ab711be'
       />
 
       <link rel='icon' href='/favicon.ico' />
