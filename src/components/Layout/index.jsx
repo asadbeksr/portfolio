0157b93b63df8853a705debfc66adf292bd6374c
asadbek <asadbeksr@gmail.com>
2022-11-06 00:07:08 +0500

    add: locomotive scroll

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,11 @@
 import { Footer } from 'components/UI/Footer/Footer';
 import { Header } from 'components/UI/Header/Header';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const Cursor = dynamic(
-  () => import('components/UI/Cursor'),
-  { ssr: false }
-);
+// const Cursor = dynamic(
+//   () => import('components/UI/Cursor'),
+//   { ssr: false }
+// );
 
 export default function Layout({ children }) {
   return (
