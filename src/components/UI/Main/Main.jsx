fdc8ba3875ece5358f6526a239d4b4945c2a74b6
bek <asadbeksr@gmail.com>
2023-01-24 18:54:01 +0500

    spline styles

--- src/components/UI/Main/Main.jsx
+++ src/components/UI/Main/Main.jsx
@@ -2,20 +2,6 @@ import { Container } from '@mui/material';
 import AText from '../Animated/Text';
 import Spline from '@splinetool/react-spline';
 
-export default function SplineView() {
-  return (
-    <Spline
-      style={{
-        position: 'absolute',
-        bottom: 100,
-        right: 100
-      }}
-      id='magicalBlobs'
-      scene='https://prod.spline.design/QoUsqkLcAPNMg1pz/scene.splinecode'
-    />
-  );
-}
-
 export function Main() {
   return (
     <Container>
@@ -23,20 +9,14 @@ export function Main() {
         <AText text='Asadbek Savronov' variant='h3' dir='up' />
         <AText text='Creative' variant='h3' dir='up' delay='1' />
         <AText text='Front-end Developer' variant='h3' dir='up' delay='1.5' />
-        <AText
-          text='based in Tashkent'
-          variant='h3'
-          dir='up'
-          delay='2.3'
-        />
-        <AText
-          text='Uzbekistan'
-          variant='h3'
-          dir='up'
-          delay='3'
-        />
+        <AText text='based in Tashkent' variant='h3' dir='up' delay='2.3' />
+        <AText text='Uzbekistan' variant='h3' dir='up' delay='3' />
       </div>
-      <SplineView />
+
+      <Spline
+        id='hero-sphere'
+        scene='https://prod.spline.design/QoUsqkLcAPNMg1pz/scene.splinecode'
+      />
     </Container>
   );
 }
