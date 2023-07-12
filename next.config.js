d273be5e28e463391ac51cc94f53a96bc0bdf2bb
bek <asadbeksr@gmail.com>
2023-07-12 17:42:31 +0500

    return c-s page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
 
-// let baseUrl = 'https://development.com'
+// let  baseUrl = 'https://development.com' 
 
 // if (process.env.NEXT_PUBLIC_APP_ENV === 'production') {
 //   baseUrl = 'https://production.com'
