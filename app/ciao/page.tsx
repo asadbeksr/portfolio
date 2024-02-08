85f5b6b7620c4445d28ac13937f54fd1ef279340
asadbek <asadbeksr@gmail.com>
2024-02-08 09:59:05 +0100

    add: ciao

---
+++ app/ciao/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default async function CiaoPage() {
+  redirect('https://www.canva.com/design/DAF8JFRlegY/vriGr7v2WuJyy5CktbUYDw/view?utm_content=DAF8JFRlegY&utm_campaign=designshare');
+}
