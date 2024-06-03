b5313dc24d719be85205f848e1597aee1cd53d25
asadbek <asadbeksr@gmail.com>
2024-06-03 22:22:53 +0200

    fix: type

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -141,27 +141,27 @@ const config = {
           },
         },
         meteor: {
-          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: 1 },
-          '70%': { opacity: 1 },
+          '0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
+          '70%': { opacity: '1' },
           '100%': {
             transform: 'rotate(215deg) translateX(-500px)',
-            opacity: 0,
+            opacity: '0',
           },
         },
-        "border-beam": {
-          "100%": {
-            "offset-distance": "100%",
+        'border-beam': {
+          '100%': {
+            'offset-distance': '100%',
           },
         },
-        "shine-pulse": {
-          "0%": {
-            "background-position": "0% 0%",
+        'shine-pulse': {
+          '0%': {
+            'background-position': '0% 0%',
           },
-          "50%": {
-            "background-position": "100% 100%",
+          '50%': {
+            'background-position': '100% 100%',
           },
           to: {
-            "background-position": "0% 0%",
+            'background-position': '0% 0%',
           },
         },
       },
@@ -175,7 +175,7 @@ const config = {
         grid: 'grid 15s linear infinite',
         shimmer: 'shimmer 8s infinite',
         meteor: 'meteor 5s linear infinite',
-        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
       },
     },
   },
