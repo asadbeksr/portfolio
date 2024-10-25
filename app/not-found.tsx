1f967660ddba8a37dc0eb7e0a007cc4fb86b3f26
asadbek <asadbeksr@gmail.com>
2024-10-25 17:05:30 +0200

    upd: links

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
+'use client';
 import { Metadata } from 'next';
-import React from 'react';
-import { cn } from '@/lib/utils';
+import React, { useEffect, useState } from 'react';
 import { TextHoverEffect } from '@/components/ui/text-hover-effect';
-import { Spotlight } from '@/components/ui/Spotlight';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
@@ -40,11 +39,18 @@ const buttonMessages: string[] = [
 ];
 
 export default function NotFound() {
-  const randomMessage =
-    errorMessages[Math.floor(Math.random() * errorMessages.length)];
+  const [randomMessage, setRandomMessage] = useState('');
+  const [randomButtonMessage, setRandomButtonMessage] = useState('');
 
-  const randomButtonMessage =
-    buttonMessages[Math.floor(Math.random() * buttonMessages.length)];
+  useEffect(() => {
+    setRandomMessage(
+      errorMessages[Math.floor(Math.random() * errorMessages.length)]
+    );
+    setRandomButtonMessage(
+      buttonMessages[Math.floor(Math.random() * buttonMessages.length)]
+    );
+  }, []); // Runs once when the component mounts
+
   return (
     <div className='h-[35.4rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden'>
       {/* <Spotlight
@@ -62,7 +68,7 @@ export default function NotFound() {
         </div>
       </div>
 
-      <div className='p-4 max-w-7xl mx-auto relative z-10  w-full pt-20 md:pt-0 flex justify-center items-center flex-col gap-4'>
+      <div className='p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0 flex justify-center items-center flex-col gap-4'>
         <h1 className='text-5xl font-bold text-center leading-[1.2] md:leading-[1.4] bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 to-neutral-600 dark:from-neutral-50 dark:to-neutral-300'>
           {randomMessage}
         </h1>
